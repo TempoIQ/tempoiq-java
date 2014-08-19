e5fe609b372f44576ac69d21028fb488a47e1241
Matt Meshulam <matt.meshulam@tempo-db.com>
2014-08-19 16:49:04 -0500

    Comment out unimplemented functionality

---
+++ client-interface/src/com/tempoiq/analytics/operations/InterpolationFunction.java
@@ -0,0 +1,20 @@
+package com.tempoiq.analytics.operations;
+
+
+/**
+ *  Interpolation function
+ *  @since 1.1.0
+ */
+public enum InterpolationFunction {
+  /**
+   *  Zero-order-hold Interpolation
+   *  @since 1.1.0
+   */
+  ZOH,
+
+  /**
+   *  Linear Interpolation
+   *  @since 1.1.0
+   */
+  LINEAR
+}
