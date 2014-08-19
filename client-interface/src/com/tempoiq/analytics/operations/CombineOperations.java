ca2afa4e3fd069f6c4366641c474cc65cbecdb2f
Matt Meshulam <matt.meshulam@tempo-db.com>
2014-08-19 14:27:22 -0500

    Rename Aggregate/Combine operations classes

---
+++ client-interface/src/com/tempoiq/analytics/operations/CombineOperations.java
@@ -0,0 +1,7 @@
+package com.tempoiq.analytics.operations;
+
+public class CombineOperations extends AggregateOperations {
+	public static CombineOperations DIFFERENCE;
+	public static CombineOperations RELATIVE_DIFFERENCE;
+	public static CombineOperations DIVIDE;
+}
